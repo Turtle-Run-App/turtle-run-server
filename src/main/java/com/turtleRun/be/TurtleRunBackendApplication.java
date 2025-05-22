655964ac625d580ba08c7a403b9ed30d5365c60c
hoho00 <jhoya963@gmail.com>
2025-05-22 16:30:25 +0900

    chore: rename application name to TurtleRunBackendApplication

--- src/main/java/com/turtleRun/be/TurtleRunBackendApplication.java
+++ src/main/java/com/turtleRun/be/TurtleRunBackendApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class TurtleRunBeApplication {
+public class TurtleRunBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TurtleRunBeApplication.class, args);
+		SpringApplication.run(TurtleRunBackendApplication.class, args);
 	}
 
 }
