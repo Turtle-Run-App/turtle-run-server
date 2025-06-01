d4e88f40877065767baa90d2b56ce870a0e404a4
hoho00 <jhoya963@gmail.com>
2025-06-01 23:29:54 +0900

    feat: add RunningSessionStatistics in Running Domain

---
+++ src/main/java/com/turtleRun/be/running/domain/RunningSessionStatistics.java
@@ -0,0 +1,40 @@
+package com.turtleRun.be.running.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "running_session_statistics")
+@Getter
+@Setter
+@NoArgsConstructor
+public class RunningSessionStatistics {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "session_id")
+    private RunningSession session;
+
+    private BigDecimal averageStrideLength;
+    private BigDecimal averageGroundContactTime;
+    private BigDecimal averageVerticalOscillation;
+    private BigDecimal averagePower;
+    private Integer averageCadence;
+    private Integer maxHeartRate;
+    private Integer averageHeartRate;
+    private BigDecimal trainingEffect;
+    private BigDecimal vo2max;
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
